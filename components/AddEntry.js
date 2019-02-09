31cbeca1819f89e7f2739377f0e545b307635019
Steve <steveorsino@gmail.com>
2019-02-09 17:18:08 -0500

    added to home method on AddEntry component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,7 @@ import { submitEntry, removeEntry } from '../utils/api';
 import { connect } from 'react-redux';
 import { addEntry } from '../actions';
 import { white, purple } from '../utils/colors';
+import { NavigationActions } from 'react-navigation';
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -66,9 +67,8 @@ class AddEntry extends Component {
     this.props.dispatch(addEntry({
       [key]: entry
     }))
-    //navigate to home
-    submitEntry({ entry, key })
-    //clear local notification
+
+    
 
     this.setState(() => ({
       run: 0,
@@ -77,6 +77,11 @@ class AddEntry extends Component {
       sleep: 0,
       eat: 0
     }))
+
+    this.toHome()
+
+    submitEntry({ entry, key })
+    //clear local notification
   }
 
   reset = () => {
@@ -84,9 +89,17 @@ class AddEntry extends Component {
     this.props.dispatch(addEntry({
       [key]: getDailyReminderValue()
     }))
-    //route to home
+
+    this.toHome()
+
     removeEntry(key);
   }
+
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({
+      key: 'AddEntry'
+    }))
+  } 
 
   render() {
     const metaInfo = getMetricMetaInfo();
