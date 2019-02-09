5771f16ab25265bbba5f053bcce434aea7289a37
Steve <steveorsino@gmail.com>
2019-02-09 12:29:12 -0500

    added navigation

--- components/History.js
+++ components/History.js
@@ -8,8 +8,12 @@ import UdaciFitnessCalendar from 'udacifitness-calendar';
 import { white } from '../utils/colors';
 import DateHeader from './DateHeader';
 import MetricCard from './MetricCard';
+import { AppLoading } from 'expo';
 
 class History extends Component {
+  state = {
+    ready: false
+  }
   componentDidMount() {
     const { dispatch } = this.props;
 
@@ -22,6 +26,9 @@ class History extends Component {
           }))
         }
       })
+      .then(() => this.setState(() => ({
+        ready: true
+      })))
   }
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
     <View style={styles.item}>
@@ -50,6 +57,12 @@ class History extends Component {
   }
   render() {
     const { entries } = this.props;
+    const { ready } = this.state;
+
+    if (ready === false) {
+      return <AppLoading />
+    }
+
     return (
       <UdaciFitnessCalendar
         items={entries}
